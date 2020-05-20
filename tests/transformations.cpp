45a92d06132cd9d373ae8018268960d7eb99113a
kz04px <6536046+kz04px@users.noreply.github.com>
2020-05-20 17:35:25 +0100

    Add test cases

--- tests/transformations.cpp
+++ tests/transformations.cpp
@@ -1,10 +1,8 @@
-#include <iostream>
 #include <libataxx/bitboard.hpp>
 #include "catch.hpp"
 
-TEST_CASE("transformation") {
-    // Horizontal
-    const std::pair<libataxx::Bitboard, libataxx::Bitboard> horizontal[] = {
+TEST_CASE("flip_horizontal()") {
+    const std::pair<libataxx::Bitboard, libataxx::Bitboard> bitboards[] = {
         {libataxx::bitboards::Empty, libataxx::bitboards::Empty},
         {libataxx::bitboards::All, libataxx::bitboards::All},
         {libataxx::bitboards::FileA, libataxx::bitboards::FileG},
@@ -17,12 +15,13 @@ TEST_CASE("transformation") {
         {libataxx::Bitboard{libataxx::squares::B2},
          libataxx::Bitboard{libataxx::squares::F2}},
     };
-    for (const auto& [bb1, bb2] : horizontal) {
+    for (const auto& [bb1, bb2] : bitboards) {
         REQUIRE(bb1.flip_horizontal() == bb2);
     }
+}
 
-    // Vertical
-    const std::pair<libataxx::Bitboard, libataxx::Bitboard> vertical[] = {
+TEST_CASE("flip_vertical()") {
+    const std::pair<libataxx::Bitboard, libataxx::Bitboard> bitboards[] = {
         {libataxx::bitboards::Empty, libataxx::bitboards::Empty},
         {libataxx::bitboards::All, libataxx::bitboards::All},
         {libataxx::bitboards::FileA, libataxx::bitboards::FileA},
@@ -35,12 +34,13 @@ TEST_CASE("transformation") {
         {libataxx::Bitboard{libataxx::squares::B2},
          libataxx::Bitboard{libataxx::squares::B6}},
     };
-    for (const auto& [bb1, bb2] : vertical) {
+    for (const auto& [bb1, bb2] : bitboards) {
         REQUIRE(bb1.flip_vertical() == bb2);
     }
+}
 
-    // Diagonal A7G1
-    const std::pair<libataxx::Bitboard, libataxx::Bitboard> a7g1[] = {
+TEST_CASE("flip_diagA7G1()") {
+    const std::pair<libataxx::Bitboard, libataxx::Bitboard> bitboards[] = {
         {libataxx::bitboards::Empty, libataxx::bitboards::Empty},
         {libataxx::bitboards::All, libataxx::bitboards::All},
         {libataxx::bitboards::FileA, libataxx::bitboards::Rank7},
@@ -53,12 +53,13 @@ TEST_CASE("transformation") {
         {libataxx::Bitboard{libataxx::squares::B2},
          libataxx::Bitboard{libataxx::squares::F6}},
     };
-    for (const auto& [bb1, bb2] : a7g1) {
+    for (const auto& [bb1, bb2] : bitboards) {
         REQUIRE(bb1.flip_diagA7G1() == bb2);
     }
+}
 
-    // Diagonal A1G7
-    const std::pair<libataxx::Bitboard, libataxx::Bitboard> a1g7[] = {
+TEST_CASE("flip_diagA1G7()") {
+    const std::pair<libataxx::Bitboard, libataxx::Bitboard> bitboards[] = {
         {libataxx::bitboards::Empty, libataxx::bitboards::Empty},
         {libataxx::bitboards::All, libataxx::bitboards::All},
         {libataxx::bitboards::FileA, libataxx::bitboards::Rank1},
@@ -71,12 +72,13 @@ TEST_CASE("transformation") {
         {libataxx::Bitboard{libataxx::squares::B6},
          libataxx::Bitboard{libataxx::squares::F2}},
     };
-    for (const auto& [bb1, bb2] : a1g7) {
+    for (const auto& [bb1, bb2] : bitboards) {
         REQUIRE(bb1.flip_diagA1G7() == bb2);
     }
+}
 
-    // Rotate 90 clockwise
-    const std::pair<libataxx::Bitboard, libataxx::Bitboard> rot90[] = {
+TEST_CASE("rot90()") {
+    const std::pair<libataxx::Bitboard, libataxx::Bitboard> bitboards[] = {
         {libataxx::bitboards::Empty, libataxx::bitboards::Empty},
         {libataxx::bitboards::All, libataxx::bitboards::All},
         {libataxx::bitboards::FileA, libataxx::bitboards::Rank7},
@@ -89,12 +91,13 @@ TEST_CASE("transformation") {
         {libataxx::Bitboard{libataxx::squares::B2},
          libataxx::Bitboard{libataxx::squares::B6}},
     };
-    for (const auto& [bb1, bb2] : rot90) {
+    for (const auto& [bb1, bb2] : bitboards) {
         REQUIRE(bb1.rot90() == bb2);
     }
+}
 
-    // Rotate 180 clockwise
-    const std::pair<libataxx::Bitboard, libataxx::Bitboard> rot180[] = {
+TEST_CASE("rot180()") {
+    const std::pair<libataxx::Bitboard, libataxx::Bitboard> bitboards[] = {
         {libataxx::bitboards::Empty, libataxx::bitboards::Empty},
         {libataxx::bitboards::All, libataxx::bitboards::All},
         {libataxx::bitboards::FileA, libataxx::bitboards::FileG},
@@ -107,13 +110,13 @@ TEST_CASE("transformation") {
         {libataxx::Bitboard{libataxx::squares::B2},
          libataxx::Bitboard{libataxx::squares::F6}},
     };
-    for (const auto& [bb1, bb2] : rot180) {
-        REQUIRE(bb1.rot90().rot90() == bb2);
+    for (const auto& [bb1, bb2] : bitboards) {
         REQUIRE(bb1.rot180() == bb2);
     }
+}
 
-    // Rotate 270 clockwise
-    const std::pair<libataxx::Bitboard, libataxx::Bitboard> rot270[] = {
+TEST_CASE("rot270()") {
+    const std::pair<libataxx::Bitboard, libataxx::Bitboard> bitboards[] = {
         {libataxx::bitboards::Empty, libataxx::bitboards::Empty},
         {libataxx::bitboards::All, libataxx::bitboards::All},
         {libataxx::bitboards::FileA, libataxx::bitboards::Rank1},
@@ -126,15 +129,17 @@ TEST_CASE("transformation") {
         {libataxx::Bitboard{libataxx::squares::B2},
          libataxx::Bitboard{libataxx::squares::F2}},
     };
-    for (const auto& [bb1, bb2] : rot270) {
-        REQUIRE(bb1.rot90().rot90().rot90() == bb2);
+    for (const auto& [bb1, bb2] : bitboards) {
         REQUIRE(bb1.rot270() == bb2);
     }
+}
 
-    // Same
-    const libataxx::Bitboard same[] = {
+TEST_CASE("identity") {
+    const libataxx::Bitboard bitboards[] = {
         libataxx::bitboards::Empty,
         libataxx::bitboards::All,
+        libataxx::bitboards::Edge,
+        libataxx::bitboards::Center,
         libataxx::bitboards::FileA,
         libataxx::bitboards::FileB,
         libataxx::bitboards::FileC,
@@ -142,19 +147,63 @@ TEST_CASE("transformation") {
         libataxx::bitboards::FileE,
         libataxx::bitboards::FileF,
         libataxx::bitboards::FileG,
+        libataxx::bitboards::Rank1,
+        libataxx::bitboards::Rank2,
+        libataxx::bitboards::Rank3,
+        libataxx::bitboards::Rank4,
+        libataxx::bitboards::Rank5,
+        libataxx::bitboards::Rank6,
+        libataxx::bitboards::Rank7,
         libataxx::Bitboard{libataxx::squares::B2},
         libataxx::Bitboard{libataxx::squares::B6},
         libataxx::Bitboard{libataxx::squares::F2},
         libataxx::Bitboard{libataxx::squares::B6},
     };
-    for (const auto& bb : same) {
+    for (const auto& bb : bitboards) {
         REQUIRE(bb.flip_horizontal().flip_horizontal() == bb);
         REQUIRE(bb.flip_vertical().flip_vertical() == bb);
         REQUIRE(bb.flip_diagA1G7().flip_diagA1G7() == bb);
         REQUIRE(bb.flip_diagA7G1().flip_diagA7G1() == bb);
         REQUIRE(bb.rot90().rot90().rot90().rot90() == bb);
+        REQUIRE(bb.rot90().rot90().rot180() == bb);
+        REQUIRE(bb.rot90().rot180().rot90() == bb);
+        REQUIRE(bb.rot180().rot90().rot90() == bb);
         REQUIRE(bb.rot180().rot180() == bb);
         REQUIRE(bb.rot270().rot270().rot270().rot270() == bb);
         REQUIRE(bb.rot270().rot90() == bb);
+        REQUIRE(bb.rot90().rot270() == bb);
+    }
+}
+
+TEST_CASE("equivalency") {
+    const libataxx::Bitboard bitboards[] = {
+        libataxx::bitboards::Empty,
+        libataxx::bitboards::All,
+        libataxx::bitboards::Edge,
+        libataxx::bitboards::Center,
+        libataxx::bitboards::FileA,
+        libataxx::bitboards::FileB,
+        libataxx::bitboards::FileC,
+        libataxx::bitboards::FileD,
+        libataxx::bitboards::FileE,
+        libataxx::bitboards::FileF,
+        libataxx::bitboards::FileG,
+        libataxx::bitboards::Rank1,
+        libataxx::bitboards::Rank2,
+        libataxx::bitboards::Rank3,
+        libataxx::bitboards::Rank4,
+        libataxx::bitboards::Rank5,
+        libataxx::bitboards::Rank6,
+        libataxx::bitboards::Rank7,
+        libataxx::Bitboard{libataxx::squares::B2},
+        libataxx::Bitboard{libataxx::squares::B6},
+        libataxx::Bitboard{libataxx::squares::F2},
+        libataxx::Bitboard{libataxx::squares::B6},
+    };
+    for (const auto& bb : bitboards) {
+        REQUIRE(bb.rot90().rot90() == bb.rot180());
+        REQUIRE(bb.rot90().rot90().rot90() == bb.rot270());
+        REQUIRE(bb.rot180().rot90() == bb.rot270());
+        REQUIRE(bb.rot90().rot180() == bb.rot270());
     }
 }
